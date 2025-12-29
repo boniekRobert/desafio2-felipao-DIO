e0a75dc3c470ca2a14450849d31b4b2b9c9d4d17
boniekRobert <robertboniek94@gmail.com>
2025-12-29 15:21:56 -0300

    desafio finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("Hello, world");
+function saldoVitorias(vitorias, derrotas) {
+    return vitorias - derrotas
+}
+
+let saldo = saldoVitorias(10, 5);
+
+let nivel;
+
+if (saldo < 10) {
+    nivel = "Ferro";    
+} else if (saldo <= 20) {
+    nivel = "Bronze";
+} else if (saldo <= 50) {
+    nivel = "Prata";
+} else if (saldo <= 80) {
+    nivel = "Ouro";
+} else if (saldo <= 90) {
+    nivel = "Diamante";
+} else if (saldo <= 100) {
+    nivel = "Lendário";
+} else {
+    nivel = "Imortal";
+}
+
+console.log(`O Herói tem de saldo de vitórias ${saldo} está no nível de ${nivel}`);
